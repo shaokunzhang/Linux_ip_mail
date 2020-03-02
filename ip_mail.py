189e3445a95184d5d9d75af0dce43f9d9e509099
unknown <13482918188@163.com>
2020-03-02 13:17:11 +0800

    百度

--- ip_mail.py
+++ ip_mail.py
@@ -1,9 +1,6 @@
 #-*-coding:utf-8-*-
 import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect(("1.1.1.1",80))
+s.connect(("www.baidu.com",80))
 ip_adress=s.getsockname()[0]
 print (ip_adress)
-
-
-  
