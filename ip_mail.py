f1dd7b5d82800e09dba408f6dc4f4a8ebd54cb82
unknown <13482918188@163.com>
2020-07-11 16:09:54 +0800

    xtt

--- ip_mail.py
+++ ip_mail.py
@@ -1,6 +1,5 @@
 #-*-coding:utf-8-*-
-import socket
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.connect(("www.baidu.com",80))
-ip_adress=s.getsockname()[0]
-print (ip_adress)
+def df(name,qq):
+    print(name)
+    print(qq)
+df(234,123)
---
+++ 卖xtt copy.py
@@ -0,0 +1,111 @@
+import winput            #键盘鼠标
+import sys
+import random
+import pickle
+import pyperclip
+from winput.vk_codes import *
+import time
+def copy():
+    winput.press_key(VK_CONTROL)
+    winput.click_key(VK_C)
+    winput.release_key(VK_CONTROL)
+def paste():
+    winput.press_key(VK_CONTROL)
+    winput.click_key(VK_V)
+    winput.release_key(VK_CONTROL)
+def tab():
+    winput.press_key(VK_MENU)
+    winput.click_key(VK_TAB)
+    winput.release_key(VK_MENU)
+def tab_b():
+    winput.press_key(VK_MENU)
+    winput.click_key(VK_TAB)
+    time.sleep(0.5)
+    winput.click_key(VK_TAB)
+    winput.release_key(VK_MENU)
+def ctrl_a():
+    winput.press_key(VK_CONTROL)
+    winput.click_key(VK_A)
+    winput.release_key(VK_CONTROL)
+def run(time_min,time_max,num_x,num_y,deal_x,deal_y):
+    print("程序开始")
+    time.sleep(1)                      #程序开始
+    winput.set_mouse_pos(1722, 20)     #鼠标切换EXCEL 
+    winput.click_mouse_button(1)       
+    time.sleep(0.5)
+    while 1 :                      
+        copy()                         #复制
+        Random_time=random.randint(time_min,time_max)           
+        winput.set_mouse_pos(num_x,num_y)  #鼠标选择ak数量
+        winput.click_mouse_button(1)
+        time.sleep(0.5)
+        ctrl_a()                       #全选
+        time.sleep(0.5)
+        winput.click_key(VK_DELETE)    #删除
+        time.sleep(0.5)
+        paste()                        #粘贴
+        time.sleep(0.5)
+        winput.set_mouse_pos(deal_x,deal_y)  #鼠标点击交易
+        winput.click_mouse_button(1)
+        time.sleep(1)
+        winput.set_mouse_pos(1726, 20) #切换窗口
+        winput.click_mouse_button(1)
+        time.sleep(0.5)
+        winput.click_key(VK_RIGHT)
+        pyperclip.copy(Random_time)
+        paste()       
+        winput.click_key(VK_LEFT)      #左键
+        winput.click_key(VK_DOWN)      #下键
+        time.sleep(Random_time)   
+def cs():
+    print("请键入随机时间下限")
+    timemin=input()
+    if timemin=="":
+        time_min=10
+    else:
+        time_min=int(timemin)
+    print("请键入随机时间上限")
+    timemax=input()
+    if timemax=="":
+        time_max=20
+    else:
+        time_max=int(timemax)
+    print("请键入数量坐标X")
+    numx=input()
+    if numx=="":
+        num_x=698
+    else:
+        num_x=int(numx)
+    print("请键入数量坐标Y")
+    numy=input()
+    if numy=="":
+        num_y=698
+    else:
+        num_y=int(numy)
+    print("请键入成交坐标X")
+    dealx=input()
+    if dealx=="":
+        deal_x=794
+    else:
+        deal_x=int(dealx)
+    print("请键入成交坐标Y")
+    dealy=input()
+    if dealy=="":
+        deal_y=794
+    else:
+        deal_y=int(dealy) 
+    aa=[time_min,time_max,num_x,num_y,deal_x,deal_y]
+    f=open("var.txt","wb")
+    pickle.dump(aa,f,0)
+    f.close()
+print("请选择\n","1:开始程序  2:参数调整")
+a = int(input())
+if  a==1:
+    f=open("var.txt","rb")
+    bb=pickle.load(f)
+    print(bb)
+    run(bb[0],bb[1],bb[2],bb[3],bb[4],bb[5])
+elif a==2:
+    cs()
+    
+            
