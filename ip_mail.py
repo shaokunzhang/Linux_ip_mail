b28e80c4baccd69c3988bee643ac61bb516dd7d2
zhangshaokun <13482918188@163.com>
2020-07-15 16:19:27 +0800

    7/15

--- ip_mail.py
+++ ip_mail.py
@@ -2,7 +2,7 @@
 import winput
 import time
 from winput.vk_codes import *
-winput.set_mouse_pos(1057, 344)     #鼠标切换EXCEL
+winput.set_mouse_pos(1747, 111)     #鼠标切换EXCEL
 time.sleep(0.5)
 winput.click_mouse_button(1)       
 def printtime():
@@ -11,3 +11,4 @@ def printtime():
     winput.click_key(VK_OEM_1)
     winput.release_key(VK_CONTROL)
     winput.release_key(VK_LSHIFT)
+printtime()
